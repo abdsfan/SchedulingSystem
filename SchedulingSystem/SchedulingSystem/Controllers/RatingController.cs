649ab7985474c1b57622ffcdc263a99a2119ea3f
Abdullah Yousfan <ayousfan@pdsit.net>
2017-10-04 17:04:04 -0500

    added driver score controller and view

---
+++ SchedulingSystem/SchedulingSystem/Controllers/RatingController.cs
@@ -0,0 +1,166 @@
+﻿using SchedulingSystem.Models;
+using SchedulingSystem.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SchedulingSystem.Controllers
+{
+    public class RatingController : Controller
+    {
+        public ApplicationDbContext context;
+
+        public RatingController()
+        {
+            context = new ApplicationDbContext();
+        }
+
+        // GET: Rating
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        public ActionResult SearchForDriver()
+        {
+            var dataForRating = new DataForRatingViewModel();
+            return View(dataForRating);
+
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public ActionResult SearchForDriver(DataForRatingViewModel data)
+        {
+            var cap = context.Caps.FirstOrDefault(n => n.CapNumber == data.CapNumber);
+            if(cap != null)
+            {
+                var date = context.Schedules.FirstOrDefault(n => n.Cap.Id == cap.Id && n.ShiftDate == data.DateOfDay && (n.StartTime.Hour <= data.TimeOfDay.Hour && n.EndTime.Hour >= data.TimeOfDay.Hour));
+                if(date == null)
+                {
+                    ModelState.AddModelError("", "invalid entry, Try again. ");
+                    return View(data);
+                }
+                var user = date.ApplicationUser.Id;
+
+                var driverInfo = new ScoringDriver
+                {
+                    UserId = user,
+                    CapId = cap.Id,
+                };
+
+
+                //create new view model for user id and cap id
+                //instantiate it here and assig the value to it
+                //then create the view and r
+                return RedirectToAction("ScoringDriver", driverInfo);
+            }
+
+            ModelState.AddModelError("", "invalid entry, Try again. ");
+            return View();
+        }
+
+        [AllowAnonymous]
+        public ActionResult ScoringDriver(ScoringDriver driverInfo)
+        {
+            return View(driverInfo);
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public ActionResult SaveDriverScore(ScoringDriver driverInfo)
+        {
+            if(driverInfo.DriverScore > 10)
+            {
+                //this is not working, needs fixing
+                ModelState.AddModelError("", "invalid score, Try again. ");
+                return View(driverInfo);
+            }
+
+            var user = context.Users.FirstOrDefault(u => u.Id == driverInfo.UserId);
+            var cap = context.Caps.FirstOrDefault(c => c.Id == driverInfo.CapId);
+            var newRating = new Rating
+            {
+                DateRated = DateTime.Today,
+                ApplicationUser = user,
+                Cap = cap,
+                Score = driverInfo.DriverScore
+            };
+            context.Ratings.Add(newRating);
+            context.SaveChanges();        
+            return RedirectToAction("Index", "Home");            
+        }
+
+
+        // GET: Rating/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Rating/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Rating/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Rating/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Rating/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Rating/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ SchedulingSystem/SchedulingSystem/ViewModels/RatingViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SchedulingSystem.ViewModels
+{
+    public class DataForRatingViewModel
+    {
+
+        [Required]
+        [Display(Name = "Date of incident")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}")]
+        public DateTime DateOfDay { get; set; }
+
+        [Required]
+        //[StringLength(2, ErrorMessage = "Enter only the hour")]
+        [DataType(DataType.Time)]
+        //[DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = @"{0:hh\-mm}")]
+        [Display(Name = "Hour of day in military time")]
+        public DateTime TimeOfDay { get; set; }
+
+        [Required]
+        [Display(Name = "Cap Number")]
+        [RegularExpression(@"^[0-9]{3}", ErrorMessage = "Invalid Cap Number")]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:#.#}")]
+        public int CapNumber { get; set; }
+    }
+
+    public class ScoringDriver
+    {
+        public string UserId { get; set; }
+
+        public int CapId { get; set; }
+
+        [Required]
+        [Display(Name = "Driver Score")]
+        [RegularExpression(@"^[0-9]{1,2}", ErrorMessage = "Invalid rating")]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:#.#}")]
+        public int DriverScore { get; set; }
+    }
+}
